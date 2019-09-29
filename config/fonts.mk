982491dea1f0508baaf512eac718a61067b2a027
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-29 17:46:51 +0530

    vendor: fonts: Refactor fonts.mk to include all fonts
    - It's way too tedious to type out each font's path to copy, especially since 10 introduced so many styles.
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I1c84ca37fea03ea7c832a003512701d0c48d10f8

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,9 +1,6 @@
 # Fonts
+LOCAL_PATH := vendor/flare/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
 PRODUCT_COPY_FILES += \
-    vendor/flare/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/flare/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/flare/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/flare/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/flare/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/flare/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/flare/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
