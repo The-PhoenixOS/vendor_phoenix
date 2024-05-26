6ca0164980f47468b07cae235baae2a54fba222d
Dhina17 <dhinalogu@gmail.com>
2024-05-26 15:27:50 +0530

    soong: Add libcameraservice extension config
    Change-Id: Id20f782c1008fed1f15250129618326091f61bf8

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -46,6 +46,7 @@ SOONG_CONFIG_flareGlobalVars += \
     target_health_charging_control_supports_deadline \
     target_health_charging_control_supports_toggle \
     target_init_vendor_lib \
+    target_libcameraservice_ext_lib \
     target_power_libperfmgr_mode_extension_lib \
     target_powershare_path \
     target_powershare_enabled \
@@ -113,6 +114,7 @@ BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
+TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
 TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= false
@@ -144,6 +146,7 @@ SOONG_CONFIG_flareGlobalVars_target_health_charging_control_supports_deadline :=
 SOONG_CONFIG_flareGlobalVars_target_health_charging_control_supports_toggle := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE)
 SOONG_CONFIG_flareGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_flareGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
+SOONG_CONFIG_flareGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 SOONG_CONFIG_flareGlobalVars_target_power_libperfmgr_mode_extension_lib := $(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB)
 SOONG_CONFIG_flareGlobalVars_target_powershare_path := $(TARGET_POWERSHARE_PATH)
 SOONG_CONFIG_flareGlobalVars_target_powershare_enabled := $(TARGET_POWERSHARE_ENABLED)
