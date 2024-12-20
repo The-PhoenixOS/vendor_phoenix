983ef1fde415388ce35d5f5ae8cc3e9a36f4dea0
John Galt <johngaltfirstrun@gmail.com>
2024-12-20 17:33:03 +0530

    vendor: config: Support enabling blur
    Reference:
    https://github.com/crdroidandroid/android_vendor_crdroid/blob/13.0/config/crdroid.mk#L34
    
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -90,6 +90,16 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Blur
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+endif
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \
