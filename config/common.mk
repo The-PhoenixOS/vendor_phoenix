432ff7108d3154fb1f8b51041d7c45139d925db2
afterallafk <shivamatiet2001@gmail.com>
2024-12-24 14:01:34 +0530

    vendor: Set persist.sys.sf.disable_blurs to 1
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -100,7 +100,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1
 else
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
