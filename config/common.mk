ProjectBlaze/vendor_blaze

vendor: add OTA script to delete package cache
after flashing new image delete contents of /data/system/package_cache/
to make sure they are recreated. Outdated cache files are the
reason for the famous 'resource derps'

Change-Id: Ia50b8223c07d6eeb09c404599138827be911063b
Signed-off-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
Signed-off-by: SKORPION29 <sayansarkar29.slg@gmail.com>

config/common.mk
@@ -176,6 +176,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.wm.enable_remote_keyguard_animation=0
 
+# Clean up packages cache to avoid wrong strings and resources
+PRODUCT_COPY_FILES += \
+    vendor/blaze/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI

The-PhoenixOS/vendor_phoenix

config/common.mk
@@ -196,6 +196,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 endif
 endif
 
+# Clean up packages cache to avoid wrong strings and resources
+PRODUCT_COPY_FILES += \
+    vendor/flare/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI