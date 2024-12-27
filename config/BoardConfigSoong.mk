zaidkhan0997/vendor_blaze

vendor: soong: Add camera_needs_client_info
Change-Id: I08fa7ce273d59f171b9e22e74c7ecef5e3c94866
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -56,7 +56,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
     needs_camera_boottime \
-    target_camera_package_name
+    target_camera_package_name \
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -91,6 +92,7 @@ SOONG_CONFIG_blazeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
+SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

The-PhoenixOS/vendor_phoenix

config/BoardConfigSoong.mk
@@ -59,7 +59,8 @@ SOONG_CONFIG_flareGlobalVars += \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
     needs_camera_boottime \
-    target_camera_package_name
+    target_camera_package_name \
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += flareNvidiaVars
 SOONG_CONFIG_flareNvidiaVars += \
@@ -96,6 +97,7 @@ SOONG_CONFIG_flareGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_flareGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_flareGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_flareGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
+SOONG_CONFIG_flareGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0