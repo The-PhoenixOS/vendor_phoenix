zaidkhan0997/vendor_blaze

soong: Add TARGET_INCLUDES_MIUI_CAMERA
- Inspired from 11f751a76380eca5e5bd1b5db329ff9fc4c732df

Change-Id: I72b74aa5dfcd25cda70f8b754c58fe1af38163db

config/BoardConfigSoong.mk
@@ -60,7 +60,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     camera_needs_client_info \
     uses_nothing_camera \
     uses_miui_camera \
-    uses_oppo_camera
+    uses_oppo_camera \
+    include_miui_camera
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -100,6 +101,7 @@ SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_blazeGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
+SOONG_CONFIG_blazeGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

The-PhoenixOS/vendor_phoenix

config/BoardConfigSoong.mk
@@ -63,7 +63,8 @@ SOONG_CONFIG_flareGlobalVars += \
     camera_needs_client_info \
     uses_nothing_camera \
     uses_miui_camera \
-    uses_oppo_camera
+    uses_oppo_camera \
+    include_miui_camera
 
 SOONG_CONFIG_NAMESPACES += flareNvidiaVars
 SOONG_CONFIG_flareNvidiaVars += \
@@ -105,6 +106,7 @@ SOONG_CONFIG_flareGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA
 SOONG_CONFIG_flareGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_flareGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_flareGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
+SOONG_CONFIG_flareGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0